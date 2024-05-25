a3c08411fcc28815485b1ca1b0268081b0d6cdbc
binarynoise <binarynoise@mail.de>
2024-05-25 23:34:07 +0200

    docker

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM ubuntu:22.04
+
+RUN apt-get update && apt-get install -y build-essential cmake ninja-build protobuf-compiler
+RUN useradd -u 1000 -m user
+USER user
+
+WORKDIR /home/user
+CMD bash
---
+++ run-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+set -e
+
+docker build -t aapt2-builder .
+docker run --rm -it \
+	-v .:/home/user/aapt2 \
+	-v "$ANDROID_SDK_ROOT":/home/user/Android-Sdk \
+	-e ANDROID_SDK_ROOT=:home/user/Android-Sdk \
+	-w /home/user/aapt2 \
+	aapt2-builder
